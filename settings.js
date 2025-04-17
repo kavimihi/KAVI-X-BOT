e26185b94dff5e0e5bef875897d3afb7c4aa7c2d
kavimihi <150218589+kavimihi@users.noreply.github.com>
2025-04-17 14:47:02 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,17 +1,17 @@
 const fs = require('fs')
 const chalk = require('chalk')
 
-global.sessionID = process.env.SESSIONID || ""
-global.botname = process.env.BOTNAME || ""
-global.ownernumber = process.env.OWNERNUMBER || ""
-global.ownername = process.env.OWNERNAME || ""
+global.sessionID = process.env.SESSIONID || "KAVI-X-SESSION-ID~~~GxB2GYRT#adD6RdYP-0lRaYXxd7m2NU9neSSgB6CffHwNpQg9mno"
+global.botname = process.env.BOTNAME || "KAVI-X"
+global.ownernumber = process.env.OWNERNUMBER || "94702128378"
+global.ownername = process.env.OWNERNAME || "Cyber Kavi"
 global.websitex = process.env.WEBSITEX || ""
 global.wagc = process.env.WAGC || ""
 global.botscript = process.env.BOTSCRIPT || ""
 global.packname = process.env.PACKNAME || "KAVI-X"
-global.author = process.env.AUTHOR || ""
+global.author = process.env.AUTHOR || "Cyber Kavi"
 global.creator = process.env.CREATOR || ""
-global.botprefix = process.env.BOTPREFIX || ""
+global.botprefix = process.env.BOTPREFIX || "."
 global.restart = process.env.RESTART || true
 global.mongoDB = process.env.MONGODB_URI || "mongodb+srv://kavishka:KAVImihi321@whatsapp-bot.ssmxc2a.mongodb.net/testing?retryWrites=true&w=majority&appName=whatsapp-bot"
 
